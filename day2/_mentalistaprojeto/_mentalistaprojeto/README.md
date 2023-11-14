4d1290c820ad3eade263a70915db22db198b30fc
Patrice Camati <patrice.camati@gmail.com>
2023-11-14 13:32:53 -0300

    'day2'

---
+++ day2/_mentalistaprojeto/_mentalistaprojeto/README.md
@@ -0,0 +1,4 @@
+# _Mentalista - Projeto
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/pcamati/pen/NWovVRv](https://codepen.io/pcamati/pen/NWovVRv).
+
---
+++ day2/_mentalistaprojeto/_mentalistaprojeto/dist/script.js
@@ -0,0 +1,18 @@
+var randomNumber = Math.floor(Math.random()*1000)+1;
+console.log(randomNumber);
+
+while (guess != randomNumber) {
+  var guess = prompt("Guess a number between 1 and 1000")
+  
+  if (guess == null) {
+    break
+  }
+
+  if (guess == randomNumber) {
+    alert("You guessed correctly! The number was "+randomNumber+".")
+  } else if (guess > randomNumber) {
+    alert("Your guess is bigger than the correct number. Please guess again!")
+  } else if (guess < randomNumber) {
+    alert("Your guess is smaller than the correct number. Please guess again!")
+  }
+}
